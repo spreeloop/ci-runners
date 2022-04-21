41688d9785e912f3a09ac0544b8c3e0e03f26150
pakona <pat@spreeloop.com>
2022-04-21 19:59:08 -0400

    Remove flutter install.
    These VMs seem to lack the disk space to install
    flutter and its dependencies.

--- gcp/app_engine/entrypoint.sh
+++ gcp/app_engine/entrypoint.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-FLUTTER_SDK_VERSION="2.10.4"
-OWNER="spreeloop"
-REPO="place"
+ORG="spreeloop"
 NAME="gcp-app-engine-elastic"
 GITHUB_RUNNERS_TOKEN=$(gcloud secrets versions access latest --secret="GITHUB_RUNNERS_TOKEN")
-TOKEN=$(curl -s -X POST -H "authorization: token ${GITHUB_RUNNERS_TOKEN}" "https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token" | jq -r .token)
+
+TOKEN=$(curl -s -X POST -H "authorization: token ${GITHUB_RUNNERS_TOKEN}" "https://api.github.com/orgs/${ORG}/actions/runners/registration-token" | jq -r .token)
 
 cleanup() {
   ./config.sh remove --token "${TOKEN}"
 }
 
+# Delete any runner with such name.
+curl -X DELETE -H "Accept: application/vnd.github.v3+json" -H "authorization: token ${GITHUB_RUNNERS_TOKEN}" https://api.github.com/orgs/${ORG}/actions/runners/${NAME}
+
 ./config.sh \
-  --url "https://github.com/${OWNER}/${REPO}" \
+  --url "https://github.com/${ORG}" \
   --token "${TOKEN}" \
   --name "${NAME}" \
   --unattended \
   --work _work \
-  --labels python,flutter,flutter-${FLUTTER_SDK_VERSION},gcp,app-engine
+  --labels gcp,app-engine
 
 ./runsvc.sh
 
