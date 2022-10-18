e7b0eb553fcb385967004c01b618f083a6b3a9fb
pakona <pat@spreeloop.com>
2022-10-18 11:14:08 -0400

    Switch to balanced disks and increase their size (#10)
    Co-authored-by: Deployment Bot <spreeloop-deployment-bot@users.noreply.github.com>

--- gcp/compute_engine/create_e2_medium_template.sh
+++ gcp/compute_engine/create_e2_medium_template.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-gcloud compute instance-templates create gh-runner-e2-medium-template \
+gcloud compute instance-templates create gh-runner-e2-medium-50gb-template \
     --project=spreeloop-ci-runners \
     --image-family=ubuntu-1804-lts \
     --image-project=ubuntu-os-cloud \
-    --boot-disk-type=pd-ssd \
-    --boot-disk-size=20GB \
+    --boot-disk-type=pd-balanced \
+    --boot-disk-size=50GB \
     --machine-type=e2-medium \
     --scopes=cloud-platform \
     --metadata-from-file=startup-script=startup.sh,shutdown-script=shutdown.sh
--- gcp/compute_engine/create_e2_small_template.sh
+++ gcp/compute_engine/create_e2_small_template.sh
@@ -4,7 +4,7 @@ gcloud compute instance-templates create gh-runner-e2-small-template \
     --project=spreeloop-ci-runners \
     --image-family=ubuntu-1804-lts \
     --image-project=ubuntu-os-cloud \
-    --boot-disk-type=pd-ssd \
+    --boot-disk-type=pd-balanced \
     --boot-disk-size=20GB \
     --machine-type=e2-small \
     --scopes=cloud-platform \
