f9e46b7e52e8089b77a5c14b462d1366307cecd5
nidovic <feutseludovic@gmail.com>
2024-04-30 03:09:25 +0100

    Update startup.sh

--- gcp/compute_engine/startup.sh
+++ gcp/compute_engine/startup.sh
@@ -48,9 +48,9 @@ TOKEN=$(curl -s -X POST -H "authorization: token ${GITHUB_RUNNERS_TOKEN}" "https
 
 ## Download runner software.
 mkdir /runner && cd /runner
-curl -o actions-runner-linux-x64-2.290.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.290.1/actions-runner-linux-x64-2.290.1.tar.gz
-echo "2b97bd3f4639a5df6223d7ce728a611a4cbddea9622c1837967c83c86ebb2baa  actions-runner-linux-x64-2.290.1.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
+curl -o actions-runner-linux-x64-2.316.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.0/actions-runner-linux-x64-2.316.0.tar.gz
+echo "64a47e18119f0c5d70e21b6050472c2af3f582633c9678d40cb5bcb852bcc18f  actions-runner-linux-x64-2.316.0.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-linux-x64-2.316.0.tar.gz
 
 RUNNER_ALLOW_RUNASROOT=1 /runner/config.sh \
   --url "https://github.com/${ORG}" \
